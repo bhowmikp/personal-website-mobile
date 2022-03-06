d82c3a440c092d4171e021bffb8ff6c40f74ebe8
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2022-03-05 20:02:46 -0500

    feat(setup): added routing

--- App.tsx
+++ App.tsx
@@ -4,11 +4,26 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 
 import {Test} from './src/Test';
+import {Best} from './src/Best';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <TailwindProvider utilities={utilities}>
-      <Test />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Test"
+            component={Test}
+            options={{title: 'First Screen'}}
+          />
+          <Stack.Screen name="Best" component={Best} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 };
--- src/Best.tsx
+++ src/Best.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
 
 import {useTailwind} from 'tailwind-rn';
 
-export const Test = ({navigation}: {navigation: any}): JSX.Element => {
+export const Best = (): JSX.Element => {
   const tailwind = useTailwind();
+
+  const [click, setClick] = useState<number>(0);
 
   return (
     <ScrollView style={tailwind('')}>
       <Text style={tailwind('text-blue-800 bg-yellow-700 text-[30px]')}>
-        Hello World
+        Bye World
       </Text>
 
       <View style={tailwind('flex-1 justify-center items-center')}>
+        <Text>Button clicked {click} times</Text>
+
         <View>
           <TouchableOpacity
             activeOpacity={1}
             style={tailwind('bg-white border border-black px-2')}
             onPress={(): void => {
-              navigation.navigate('Best', {});
+              setClick(click + 1);
             }}>
-            <Text style={tailwind('text-red-800')}>Navigate to Best</Text>
+            <Text style={tailwind('text-red-800')}>Click Me</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -28,4 +32,4 @@ export const Test = ({navigation}: {navigation: any}): JSX.Element => {
   );
 };
 
-export default Test;
+export default Best;
